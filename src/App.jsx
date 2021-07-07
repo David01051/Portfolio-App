da1847b08933c7f5eb3af122af449b64efd26328
David01051 <76750253+David01051@users.noreply.github.com>
2021-07-07 21:41:05 +0300

    delete app.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,14 @@ import Portfolio from './Components/portfolio/portfolio'
 import Input from './Components/Input/Input'
 import MyProject from './Components/MyProject/MyProject'
 import Footer from './Components/Footer/Footer'
-import TimeLine from './Components/TimeLine/TimeLine'
-import Button from './Components/Button/Button'
-import { UseProvider } from './Components/Context/Context'
-
-
-
-const homePageInfo = {
-  hello: 'I Am,',
-  title: 'David Talala',
-
-}
+import TimeLine from './Components/TimeLine/TimeLine';
 
 function App() {
   return (
     <UseProvider value={homePageInfo}>
       <HeaderNav />
 
-     {/* <Footer/> */}
+      {/* <Footer/> */}
 
     </UseProvider>
   );
